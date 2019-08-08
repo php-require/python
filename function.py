8f6f2b78277bd622ff618a66253bc739ef5fb00d
Jury <onsilk.ru@ya.ru>
2019-08-09 00:31:22 +0300

    write function

--- function.py
+++ function.py
@@ -1,1 +1,16 @@
-x=1
+def max2(x,y):
+	if x>y:
+		return x
+	return y
+def max3(x,y,z):
+	return max2(x, max2(y,z))
+print(max3(10,2,11))
+
+#найдет самое большое число 322
+def max2(x,y):
+	if x>y:
+		return x
+	return y
+def max3(x,y,z):
+	return max2(x, max2(y,z))
+print(max3(322,22,2)) 
